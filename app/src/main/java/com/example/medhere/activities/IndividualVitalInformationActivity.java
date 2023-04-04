3c60e3a5ed63a0a09f36bf11a6ba7b66105b2618
eeshaydv <eesha.yadav@flipkart.com>
2023-04-04 20:12:39 +0530

    Vital Dashboard UI added.

---
+++ app/src/main/java/com/example/medhere/activities/IndividualVitalInformationActivity.java
@@ -0,0 +1,16 @@
+package com.example.medhere.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.medhere.R;
+
+public class IndividualVitalInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_vital_information);
+    }
+}
